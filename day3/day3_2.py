5e0e85199e082da472cbb0968e128d1318102c86
Piotr Łach <lachpio999@gmail.com>
2021-12-03 12:43:22 +0100

    almost done, cant find bug

--- day3/day3_2.py
+++ day3/day3_2.py
@@ -1,4 +1,4 @@
-text_file = open("day3_test.txt", "r")
+text_file = open("day3.txt", "r")
 
 bits_numbers = text_file.read().split('\n')
 bits_in_number = len(bits_numbers[0]) 
@@ -19,27 +19,26 @@ for bits_number in bits_numbers: #part two
 for bit_index in range (1, bits_in_number): #from second older bit
     oxygen_ones_counter = 0
     oxygen_zeros_counter = 0
+    # print("beforeoxygenlist = ",oxygen_list, "zeros_counter = ",oxygen_zeros_counter, "ones_counter = ",oxygen_ones_counter)
     for bits_number in oxygen_list:
         if(bits_number[bit_index] == '1'): oxygen_ones_counter += 1
         elif(bits_number[bit_index] == '0'): oxygen_zeros_counter += 1
     temp_list = []
     if(len(oxygen_list) == 1): break
-    elif(oxygen_ones_counter > oxygen_zeros_counter):
+    elif(oxygen_ones_counter > oxygen_zeros_counter or oxygen_ones_counter == oxygen_zeros_counter):
         for bits_number in oxygen_list:
             if(bits_number[bit_index] == '1'): temp_list.append(bits_number)
     elif(oxygen_ones_counter < oxygen_zeros_counter):
         for bits_number in oxygen_list:
             if(bits_number[bit_index] == '0'): temp_list.append(bits_number)
-    elif(oxygen_ones_counter == oxygen_zeros_counter):
-            if(bits_number[bit_index] == '1'): 
-                temp_list.append(bits_number)
-                oxygen_list = temp_list
-                break
+
     oxygen_list = temp_list
+    # print("oxygenlist = ",oxygen_list, "zeros_counter = ",oxygen_zeros_counter, "ones_counter = ",oxygen_ones_counter)
 
 for bit_index in range (1, bits_in_number): #from second older bit
     o2_ones_counter = 0
     o2_zeros_counter = 0
+    # print("beforeO2_list = ", O2_list, "zeros_counter = ", o2_zeros_counter, "ones_counter = ", o2_ones_counter)
     for bits_number in O2_list:
         if(bits_number[bit_index] == '1'): o2_ones_counter += 1
         elif(bits_number[bit_index] == '0'): o2_zeros_counter += 1
@@ -48,15 +47,11 @@ for bit_index in range (1, bits_in_number): #from second older bit
     elif(o2_ones_counter < o2_zeros_counter):
         for bits_number in O2_list:
             if(bits_number[bit_index] == '1'): temp_list.append(bits_number)
-    elif(o2_ones_counter > o2_zeros_counter):
+    elif(o2_ones_counter > o2_zeros_counter or o2_ones_counter == o2_zeros_counter):
         for bits_number in O2_list:
             if(bits_number[bit_index] == '0'): temp_list.append(bits_number)
-    elif(o2_ones_counter == o2_zeros_counter):
-            if(bits_number[bit_index] == '0'): 
-                temp_list.append(bits_number)
-                O2_list = temp_list
-                break
     O2_list = temp_list
+    # print("O2_list = ", O2_list, "zeros_counter = ", o2_zeros_counter, "ones_counter = ", o2_ones_counter)
 
 
-print("oxygen = ", oxygen_list, "\no2 = ", O2_list)
+print("oxygen  = ", oxygen_list, "\no2\t= ", O2_list)
